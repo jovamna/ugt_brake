0385cbe0d7f45da61700c7a2617cda609454611f
“jovamna” <jovalidia.medina@gmail.com>
2022-07-13 23:10:13 +0200

    secret

--- core/settings/base.py
+++ core/settings/base.py
@@ -27,8 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-2z^%m3-@eagh!obvrz^hjgr-zy2b*8j2owl7hshuuf_mkn0e6m'
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
